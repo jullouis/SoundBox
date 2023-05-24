3d260d60865605040b5d64df5aad8f517c34262c
Noah <noah.couturier@hevs.ch>
2023-05-24 12:50:48 +0200

    Ajout du FXML admin.fxml

--- src/main/java/com/example/soundbox/Admin.java
+++ src/main/java/com/example/soundbox/Admin.java
@@ -1,26 +1,5 @@
 package com.example.soundbox;
 
-import javafx.fxml.FXML;
-
 public class Admin {
-    @FXML
-    public String addInterpreter(){
-        // foncitonnalité recherche une musique
-        return null;
-    }
-    @FXML
-    public String deleteInterpreter(){
-        // foncitonnalité recherche une musique
-        return null;
-    }
-    @FXML
-    public String addSong(){
-        // foncitonnalité recherche une musique
-        return null;
-    }
-    @FXML
-    public String deleteSong(){
-        // foncitonnalité recherche une musique
-        return null;
-    }
+    //test
 }
