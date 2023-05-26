86b171ba255205f4036b0d5c5eaa88265e2f0898
Noah <noah.couturier@hevs.ch>
2023-05-26 09:40:45 +0200

    Ajout de la classe "Album" et modification de "Song"

--- src/main/java/com/example/soundbox/HelloApplication.java
+++ src/main/java/com/example/soundbox/HelloApplication.java
@@ -18,6 +18,9 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    public HelloApplication(){
+
+    }
 
     public static void main(String[] args) {
         launch();
