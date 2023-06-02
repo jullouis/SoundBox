293c19061261021f17a5e36c8a3ec8376f692816
nathan <nathan.schindfe@hes-so.ch>
2023-06-02 11:40:48 +0200

    MAJ admin

--- src/main/java/com/example/soundbox/Admin.java
+++ src/main/java/com/example/soundbox/Admin.java
@@ -1,5 +1,22 @@
 package com.example.soundbox;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Admin extends HelloController {
-    //test
+    @FXML
+    public Label adminFonction;
+
+    @FXML
+    protected void addSong() {
+        adminFonction.setText("La musique est ajoutée");
+    }
+    @FXML
+    protected void deleteSong() {
+        adminFonction.setText("La musique est supprimée");
+    }
+    @FXML
+    protected void modSong() {
+        adminFonction.setText("La musique est modifiée");
+    }
 }
