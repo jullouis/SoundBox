24ff3d3600c01597bee39d6eb185c53011668322
nathan <nathan.schindfe@hes-so.ch>
2023-06-11 12:36:31 +0200

    MAJ admin

--- src/main/java/com/example/soundbox/Admin.java
+++ src/main/java/com/example/soundbox/Admin.java
@@ -15,13 +15,14 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import static com.example.soundbox.HelloApplication.*;
+
 public class Admin extends HelloController {
     @FXML
     public TextField adminName;
     public TextField adminYear;
     public TextField adminMainInterpreter;
     public TextField adminDuration;
-    public TextField adminFeat;
     public TextField adminAlbum;
     public ImageView adminPicture;
     public ImageView adminMP3;
@@ -48,27 +49,33 @@ public class Admin extends HelloController {
     }
     @FXML
     protected void addSong() throws IOException {
-        FileWriter writer = new FileWriter("data/database/songs_db.csv",true);
+        FileWriter writer = new FileWriter("data/database/songs_db.csv", true);
         writer.append("\n");
         writer.append("song");
         writer.append(";");
         writer.append(adminName.getText());
+        //HelloApplication.nameList.add(adminName.getText());
         writer.append(";");
         writer.append(adminAlbum.getText());
+        //HelloApplication.albumList.add(adminAlbum.getText());
         writer.append(";");
         writer.append(adminYear.getText());
+        //HelloApplication.yearList.add(adminYear.getText());
         writer.append(";");
         writer.append(adminMainInterpreter.getText());
+        //HelloApplication.mainInterpreterList.add(adminMainInterpreter.getText());
         writer.append(";");
         writer.append(adminPicture.getImage().getUrl());
+        //HelloApplication.coverUrlList.add(adminPicture.getImage().getUrl());
         writer.append(";");
         writer.append(adminDuration.getText());
+        //HelloApplication.durationList.add(adminDuration.getText());
         writer.append(";");
         writer.append("");
         writer.append(";");
         writer.append("");
         writer.append(";");
-        writer.append(adminFeat.getText());
+        writer.append("");
         writer.append(";");
         writer.append(adminMP3.getImage().getUrl());
         writer.close();
@@ -77,26 +84,93 @@ public class Admin extends HelloController {
     }
     @FXML
     protected void deleteSong() throws IOException {
-        FileWriter writer =  new FileWriter("data/database/songs_db.csv",false);
-        adminName.setText("");
-        adminAlbum.setText("");
-        adminYear.setText("");
-        adminMainInterpreter.setText("");
-        //adminPicture.setText("");
-        adminDuration.setText("");
-        adminFeat.setText("");
-        //adminMP3.setText("");
+        FileWriter writer = new FileWriter("data/database/songs_db.csv", false);
+        writer.append("");
+        HelloApplication.nameList.remove(index);
+        HelloApplication.albumList.remove(index);
+        HelloApplication.yearList.remove(index);
+        HelloApplication.mainInterpreterList.remove(index);
+        HelloApplication.coverUrlList.remove(index);
+        HelloApplication.durationList.remove(index);
         writer.close();
+        FileWriter writer1 = new FileWriter("data/database/songs_db.csv", true);
+        int i = -1;
+        while (i<= nameList.size()){
+            i ++;
+            writer1.append("song");
+            writer1.append(";");
+            writer1.append(HelloApplication.nameList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.albumList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.yearList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.mainInterpreterList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.coverUrlList.get(i);
+            writer1.append(";");
+            writer1.append(HelloApplication.durationList.get(i));
+            writer1.append(";");
+            writer1.append("");
+            writer1.append(";");
+            writer1.append("");
+            writer1.append(";");
+            writer1.append("");
+            writer1.append(";");
+            writer1.append("adminMP3");
+            writer1.append("\n");
+            writer1.close();
+        }
     }
     @FXML
-    protected void modSong() {
+    protected void modSong() throws IOException {
+        FileWriter writer = new FileWriter("data/database/songs_db.csv", false);
+        writer.append("");
+        HelloApplication.nameList.set(index,adminName.getText());
+        HelloApplication.albumList.set(index,adminAlbum.getText());
+        HelloApplication.yearList.set(index,adminYear);
+        HelloApplication.mainInterpreterList.set(index,adminMainInterpreter);
+        HelloApplication.coverUrlList.set(index,adminPicture);
+        HelloApplication.durationList.set(index,adminDuration);
+        writer.close();
+        FileWriter writer1 = new FileWriter("data/database/songs_db.csv", true);
+        int i = -1;
+        while (i<= nameList.size()){
+            i++;
+            writer1.append("song");
+            writer1.append(";");
+            writer1.append(HelloApplication.nameList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.albumList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.yearList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.mainInterpreterList.get(i));
+            writer1.append(";");
+            writer1.append(HelloApplication.coverUrlList.get(i);
+            writer1.append(";");
+            writer1.append(HelloApplication.durationList.get(i));
+            writer1.append(";");
+            writer1.append("");
+            writer1.append(";");
+            writer1.append("");
+            writer1.append(";");
+            writer1.append("");
+            writer1.append(";");
+            writer1.append("adminMP3");
+            writer1.append("\n");
+            writer1.close();
+        }
+
     }
+
     @FXML
     public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 }
+
