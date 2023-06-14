9fa8c9d6fb6775c417a1acb36039b6922eae6fda
Noah <noah.couturier@hevs.ch>
2023-06-14 14:28:01 +0200

    Mise à jour de la JavaDoc

--- src/main/java/com/example/soundbox/Login.java
+++ src/main/java/com/example/soundbox/Login.java
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
