ac599e3b8e61b408269bb9d2a33a86a4b03db3d7
Noah <noah.couturier@hevs.ch>
2023-06-14 14:26:40 +0200

    Mise à jour de la JavaDoc

--- src/main/java/com/example/soundbox/Login.java
+++ src/main/java/com/example/soundbox/Login.java
@@ -36,7 +36,7 @@ public class Login {
     ArrayList<String> adminList = new ArrayList<>();
 
     /**
-     *
+     * Ajout des utilisateurs autorisés au niveau administrateur
      */
     public Login(){
         adminList.add("Noah");
@@ -58,16 +58,16 @@ public class Login {
     protected void loginButton(){
         String id;
         String pw;
-
-        //récupérer l'id et le pw dans les champs
-        id = idField.getText();
-        pw = pwField.getText();
         int i = 0;
 
-        //voir si l'id existe
+        // Pick id and pw in the fields
+        id = idField.getText();
+        pw = pwField.getText();
+
+        //Look if id is in the userList
         if(HelloApplication.getUserList().contains(id)) {
             System.out.println(id + " " + pw);
-            //si oui, comparer au pw
+            //If id is in userList, compare with is pw
             while(i < HelloApplication.getUserList().size()){
                 if (id.equals(HelloApplication.getUserList().get(i))){
                     System.out.println(HelloApplication.getUserList().get(i));
@@ -78,28 +78,31 @@ public class Login {
                     System.out.println("Searching...");
                 }
             }
-            //si pw ok, accepter
+
+            //If pw is correct, accept access
             if (pw.equals(HelloApplication.getUserPasswords().get(i))){
-                System.out.println("Accepté");
+                System.out.println("Access accepted");
                 login.setVisible(false);
                 register.setVisible(false);
                 if (adminList.contains(id)){
                     admin.setVisible(true);
                 }
                 user.setVisible(true);
-            }else{ //si non, rejeter pour mauvais pw
+            //If pw is not correct, denied access
+            }else{
                 warningText.setText("Mot de passe incorrect");
-                System.out.println("Mot de passe incorrect");
+                System.out.println("Incorrect password");
             }
-            // si non, rejeter pour compte inexistant
+        //If id isn't in userList, reject for inexistant account
         }else{
             warningText.setText("Compte inexistant");
-            System.out.println("Compte inexistant");
+            System.out.println("Inexistant account");
         }
     }
+
     @FXML
     public void onEnter(ActionEvent ae){
-        System.out.println("test") ;
+        System.out.println("ENTER key pressed") ;
         loginButton();
     }
 
